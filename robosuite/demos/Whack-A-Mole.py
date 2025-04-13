76d7837d7671ac0bd389900d85fb6d554e1f68cb
harinik961 <harinik961@gmail.com>
2025-04-13 11:43:55 -0500

    got rid of random action hopefully

--- robosuite/demos/Whack-A-Mole.py
+++ robosuite/demos/Whack-A-Mole.py
@@ -35,13 +35,30 @@ if __name__ == "__main__":
     for i in range(10000):
         start = time.time()
         cube_pos = env.sim.data.body_xpos[env.cube_body_id]
-        eef_pos = #TODO 
-        direction = cube_pos - eef_pos
+        eef_pos = env.robots[0]._hand_pos
+
+
+
+        eef_pos = np.array(env.robots[0]._hand_pos["right"])
+        cube_pos = env.sim.data.body_xpos[env.cube_body_id]
+        #print("cube_pos:", cube_pos, type(cube_pos))
+        #print("eef_pos:", eef_pos, type(eef_pos))
+        direction = eef_pos- cube_pos
+        direction = 0.05 * direction / np.linalg.norm(direction)
+
+        action = np.zeros(env.action_spec[0].shape)
+        action[:3] = direction
+        action[3] = 0.0  # keep gripper open
+
+
+
+        #gripper_pos = env.sim.data.get_site_xpos(robot.gripper.important_sites["grip_site"])
+        #direction = cube_pos - eef_pos
         #is this already done for us??
-        direction = 0.05 * direction / np.linalg.norm(direction) # basically square root of (x² + y² + z²) to get the norm
-        action = np.zeros(env.action_spec[0].shape) #this basically zeros it
-        action[:3] = direction  # move toward object by setting the first 3 to x,y,z directions
-        action[3] = 0.0 # i dont think we need this line casue it controls the gripper
+        #direction = 0.05 * direction / np.linalg.norm(direction) # basically square root of (x² + y² + z²) to get the norm
+        #action = np.zeros(env.action_spec[0].shape) #this basically zeros it
+        #action[:3] = direction  # move toward object by setting the first 3 to x,y,z directions
+        #action[3] = 0.0 # i dont think we need this line casue it controls the gripper
         #initial code below
         #action = np.random.randn(*env.action_spec[0].shape) #TODO: set the actions
         obs, reward, done, _ = env.step(action)
